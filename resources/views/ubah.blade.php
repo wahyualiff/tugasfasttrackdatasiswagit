e1da74fc47edb1d3dd0a500c68756282688766e4
wahyualif <wahyualief1@gmail.com>
2021-06-27 21:47:55 +0700

    selected hak akses done

--- resources/views/ubah.blade.php
+++ resources/views/ubah.blade.php
@@ -14,8 +14,11 @@
                     Nama <input type="text" name="name" required value="{{$users->name}}">
                     Email <input type="email" name="email" required value="{{$users->email}}" >
                     Password <input type="text" name="password" required value="{{$users->password}}" >
-                    Hak Akses <input type="text" name="login_role" required value="{{$users->login_role}}" >
-                    
+                    Hak Akses
+                    <select name="login_role" id="">
+                        <option value="siswa" {{($users->role == "siswa") ? "selected" : ""}}>Siswa</option>
+                        <option value="guru" {{($users->role == "guru") ? "selected" : ""}}>Guru</option>
+                    </select>
                 </ul>
                 <hr>
                 <a href="{{route('users.index')}}" class="btn btn-primary">Kembali</a>
